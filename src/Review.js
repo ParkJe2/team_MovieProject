05da4cf2dc1a36ecf4143a10cbe39f6b7c2b83de
podoDJ <ourstar1345@gmail.com>
2023-06-09 11:46:30 +0900

    modifyupdate

--- src/Review.js
+++ src/Review.js
@@ -31,7 +31,6 @@ let addReviewData = new addReview();
   // console.log(new addReview());
 });
 //+++++++++++++++++++++++동준+++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-<<<<<<< HEAD
 // (3) addReview객체를 로컬스토리지에 push하는 함수. addReviewData는 new addReview를 담은 변수
 function addCollection(addReviewData) {
   reviewCollection.push(addReviewData)
@@ -184,33 +183,9 @@ function updateReview(text, idx) {
 window.addEventListener("load", () => {
   // 페이지가 열였을 때 리뷰 폼 숨기기
   reviewForm.style.display = "none";
-
-
   displayReviews();
 });
 
-//++++++++++++++++++++++++++++++제이님++++++++++++++++++++++++++++++++++++++++//
-=======
-
-// console.log(Object.keys(localStorage).map((l) => ({ ...JSON.parse(localStorage.getItem(l)), key: l })));
-// 로컬스토리 안에 저장된 리스트값 가져오기 / Object.keys(localStorage)
-// 배열은 forEach로 돌릴 수 없기에 새 객체로 반환하는 map 사용 / .map()
-// 한국 현재 시간 기준으로 정렬 / sort()
-
-Object.keys(localStorage)
-  .map((l) => ({ ...JSON.parse(localStorage.getItem(l)), key: l }))
-  .sort((a, b) => new Date(b.date) - new Date(a.date))
-  .forEach((data) => {
-    if (data.movieId === movieId) {
-      reviewList.innerHTML += `<div class="list-box">
-    <p class="list-text">${data.reviewText}</p>
-    <h5 class="list-title">⎯ ${data.nickname}</h5>
-    <p class="list-time">작성시간 : ${new Date(data.date).toLocaleString("ko-KR")}</p>
-    </div>`;
-    }
-  });
->>>>>>> f32ef43d2a7e83f3d0b3d39c19c120dc5ad9a55e
-//++++++++++++++++++++++++++++++제이님+++++++++++++++++++++++++++++++++++++++++//
 reviewWriteBtn.addEventListener("click", (event) => {
   event.preventDefault();
   // review-write-btn 버튼 클릭 시 새로고침 막기
@@ -225,16 +200,3 @@ password.addEventListener("keyup", (event) => {
 });
 // password에서 Enter키 클릭 시 reviewBtn 클릭 동작 이벤트
 // 엔터키(key code 13)가 입력되었을 경우 reviewBtn 클릭 실행
-
-class addReview {
-  constructor() {
-    this.movieId = new URLSearchParams(location.search).get("id");
-    // 현재 페이지의 url에 있는 파라미터 값(id) 가져오기
-    this.nickname = nickname.value;
-    this.password = password.value;
-    this.reviewText = reviewText.value;
-    this.date = new Date();
-    // 현재 날짜 및 시간을 한국 기준으로 가져오기
-  }
-} // class 객체 생성하여 가져가서 쓰기
-// class 실행 시 앞에 new를 붙여줘야 함
